a1765b48ceefbdccd8618366220f14d199d4a3b1
dplopezsioux <38842757+dplopezsioux@users.noreply.github.com>
2020-10-18 11:29:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-import "bootswatch/dist/darkly/bootswatch.min.css";
+import "bootswatch/dist/darkly/bootstrap.min.css";
 
 const stripePromise = loadStripe(
   "pk_test_51HS5e3ANxbOptkcMrMxGYN3B4rTSQ9zaUUpazfEsGeWzvIDWkM8KNZcbojxb3DIa8i0Cdk1cBF14l3Ti26V1eTyd00a4X0GkdY"
@@ -29,8 +29,14 @@ const MyCheckoutForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement></CardElement>
+    <form onSubmit={handleSubmit} className="card card-body">
+      <img
+        src="https://www.cstatic-images.com/car-pictures/xl/usd10cht279b021001.png"
+        class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
+        alt=""
+      ></img>
+
+      <CardElement />
       <button>BUY</button>
     </form>
   );
@@ -39,7 +45,13 @@ const MyCheckoutForm = () => {
 function App() {
   return (
     <Elements stripe={stripePromise}>
-      <MyCheckoutForm />
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <MyCheckoutForm />
+          </div>
+        </div>
+      </div>
     </Elements>
   );
 }
