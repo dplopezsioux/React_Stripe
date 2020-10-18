c77911633b150af5fc4e8ecdf683df85584329c2
dplopezsioux <38842757+dplopezsioux@users.noreply.github.com>
2020-10-18 00:24:39 -0400

    install bootswatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+import "bootswatch/dist/darkly/bootswatch.min.css";
+
+const stripePromise = loadStripe(
+  "pk_test_51HS5e3ANxbOptkcMrMxGYN3B4rTSQ9zaUUpazfEsGeWzvIDWkM8KNZcbojxb3DIa8i0Cdk1cBF14l3Ti26V1eTyd00a4X0GkdY"
+);
+
+const MyCheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.priventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement></CardElement>
+      <button>BUY</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <MyCheckoutForm />
+    </Elements>
   );
 }
 
